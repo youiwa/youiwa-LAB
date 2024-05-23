e59bd87446a42de0ce38e5a083a5770bc15d7925
Yohei Iwasaki <35583461+youiwa@users.noreply.github.com>
2024-05-23 14:08:37 +0900

    Update install.sh

--- Thonny/install.sh
+++ Thonny/install.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+#
+# install.sh
+#
+# Author:
+#     Yohei Iwasaki
+#      (National Institute of Technology, Kochi College)
+# ============
+# Script to install Thonny in Linux environment (Debian)
+# 
+# Usage:
+#     install.sh
+
 cd ~
-# Linux環境のアップデート
+# Linux Update
 sudo apt update
 sudo apt upgrade -y
 
-# 必要なソフトウェアのインストール
+# Install required software
 sudo apt install nano vim
 sudo apt install gcc make curl
 sudo apt install build-essential libssl-dev zlib1g-dev
@@ -11,21 +24,21 @@ sudo apt install libbz2-dev libreadline-dev libsqlite3-dev
 sudo apt install libncursesw5-dev xz-utils tk-dev
 sudo apt install libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-# pyenvのインストールと設定
+# Install pyenv
 curl https://pyenv.run | bash
 cp .bashrc .bashrc.orig
 sed -i '$a export PYENV_ROOT="$HOME/.pyenv"\n[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"
 
-# Pythonのアップデート
+# Python Update
 pyenv update
 pyenv install 3.12.3
 pyenv global 3.12.3
 
-# pipのインストール
+# Install pip
 sudo apt install python3-pip
 pip3 install --upgrade pip
 
-# Thonnyのインストール
+# Install Thonny
 pip3 install thonny
 
 # インストールの確認

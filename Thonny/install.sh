77c0d0e1fd0c019c071c7e6ba40f13df4dac2bb9
Yohei Iwasaki <35583461+youiwa@users.noreply.github.com>
2024-05-23 14:17:05 +0900

    Update install.sh

--- Thonny/install.sh
+++ Thonny/install.sh
@@ -14,20 +14,15 @@
 cd ~
 # Linux Update
 sudo apt update
-sudo apt upgrade -y
+sudo apt -y upgrade
 
 # Install required software
-sudo apt install nano vim
-sudo apt install gcc make curl
-sudo apt install build-essential libssl-dev zlib1g-dev
-sudo apt install libbz2-dev libreadline-dev libsqlite3-dev
-sudo apt install libncursesw5-dev xz-utils tk-dev
-sudo apt install libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt -y install nano vim gcc make curl build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # Install pyenv
 curl https://pyenv.run | bash
 cp .bashrc .bashrc.orig
-sed -i '$a export PYENV_ROOT="$HOME/.pyenv"\n[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"
+sed -i '$a \nexport PYENV_ROOT="$HOME/.pyenv"\n[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init -)"' ~/.bashrc
 
 # Python Update
 pyenv update

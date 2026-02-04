99a428e02947524aa3087e65929794222f133f32
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 15:34:56 +0900

    Update web_check.sh to download and configure index.html

--- IEE1/RaspberryPi_Setup/web_check.sh
+++ IEE1/RaspberryPi_Setup/web_check.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/bash
 
+set -x
+
+wget https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/index.html
+sudo cp index.html /var/www/html
+curl localhost
+
+sudo cp index.html $HOME/public_html/
+sed -i 's/Server/Server-UserDir/g' ~/public_html/index.html
+curl localhost/~$USER/
+
+set +x

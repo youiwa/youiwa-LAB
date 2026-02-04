4382806d77cca2fbca1ed437b10052aa2689cd65
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 16:44:12 +0900

    Update web_check.sh to use wget options and add checks

--- IEE1/RaspberryPi_Setup/web_check.sh
+++ IEE1/RaspberryPi_Setup/web_check.sh
@@ -3,21 +3,21 @@
 set -x
 
 # サーバ確認用のファイルをコピー
-wget https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/index.html
+wget -N -O index.html https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/index.html
 sudo cp index.html /var/www/html
 
 # ユーザディレクトリ確認用のファイルをコピー
 sudo cp index.html $HOME/public_html/
 sed -i 's/Server/Server-UserDir/g' $HOME/public_html/index.html
 
+# PHPファイルをコピー
+wget -N -O test.php https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/test.php
+sudo cp test.php /var/www/html
+sudo cp test.php $HOME/public_html/
+
 # Webサーバ確認
 curl localhost
 curl localhost/~$USER/
-
-# PHPファイルをコピー
-wget https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/test.php
-sudo cp test.php /var/www/html
-sudo cp test.php $HOME/public_html/
 
 # PHPの確認
 curl localhost/test.php

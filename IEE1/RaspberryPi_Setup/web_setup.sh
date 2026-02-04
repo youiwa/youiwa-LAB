5c2e4a9c923ec63b502676d5db207cf46acb453d
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 16:35:26 +0900

    Add PHP installation and configuration steps
    Enable PHP in user directory and backup configuration.

--- IEE1/RaspberryPi_Setup/web_setup.sh
+++ IEE1/RaspberryPi_Setup/web_setup.sh
@@ -23,6 +23,16 @@ chmod 711 $HOME
 # PHPのインストール
 sudo apt install php -y
 # sudo apt install php-fpm
+
+# ユーザディレクトリでPHPを有効化
+## phpのバージョンを抽出（例：8.4）
+buff=$(php -v | head -n 1 | awk -F' ' '{print $2}' | cut -d'.' -f1-2)
+## 設定ファイル(php8.4.conf)のバックアップ
+sudo cp /etc/apache2/mods-available/php$buff.conf /etc/apache2/mods-available/php$buff.conf.$(date +%Y%m%d)
+## 設定ファイルの修正
+sudo sed -i 's/php_admin_flag engine Off/# php_admin_flag engine Off/g' /etc/apache2/mods-available/php$buff.conf
+
+# Webサーバ再起動
 sudo systemctl restart apache2
 
 sudo apt autoremove -y

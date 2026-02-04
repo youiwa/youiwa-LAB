fd9e9bb39d545ae23c85151a4c338274bd9f1394
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 15:42:04 +0900

    Update web_check.sh for user directory setup
    Added user directory confirmation and modified index.html for user-specific server.

--- IEE1/RaspberryPi_Setup/web_check.sh
+++ IEE1/RaspberryPi_Setup/web_check.sh
@@ -2,12 +2,16 @@
 
 set -x
 
+# サーバ確認用のファイルをコピー
 wget https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/index.html
 sudo cp index.html /var/www/html
-curl localhost
 
+# ユーザディレクトリ確認用のファイルをコピー
 sudo cp index.html $HOME/public_html/
 sed -i 's/Server/Server-UserDir/g' $HOME/public_html/index.html
+
+# 確認
+curl localhost
 curl localhost/~$USER/
 
 set +x

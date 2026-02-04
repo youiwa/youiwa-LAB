ea0c20a4f7be01b17b0fea5c5e74c5a58c6b7410
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 15:52:07 +0900

    Add PHP test file for system information display

---
+++ IEE1/RaspberryPi_Setup/test.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    Today is <?php␣echo␣date('Y年m月d日'); ␣?>です。
+    <h2>PHPのシステム情報表示</h2>
+<?php␣phpinfo();␣?>
+</body>

ffaa7e04e383ee226ac9b550d367313c2455b3ff
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 16:09:46 +0900

    Add PHP file setup and verification to web_check.sh
    Added commands to download and copy a PHP file for testing.

--- IEE1/RaspberryPi_Setup/web_check.sh
+++ IEE1/RaspberryPi_Setup/web_check.sh
@@ -10,8 +10,16 @@ sudo cp index.html /var/www/html
 sudo cp index.html $HOME/public_html/
 sed -i 's/Server/Server-UserDir/g' $HOME/public_html/index.html
 
-# 確認
+# Webサーバ確認
 curl localhost
 curl localhost/~$USER/
 
-set +x
+# PHPファイルをコピー
+wget https://raw.githubusercontent.com/youiwa/youiwa-LAB/refs/heads/master/IEE1/RaspberryPi_Setup/test.php
+sudo cp test.php /var/www/html
+sudo cp test.php $HOME/public_html/
+
+# PHPの確認
+curl localhost/test.php
+curl localhost/~$USER/test.php
+

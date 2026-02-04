3b6f64678b750c11fb9343166333872cbb392847
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 15:49:01 +0900

    Add basic PHP HTML structure and info display

--- IEE1/RaspberryPi_Setup/ex01.php
+++ IEE1/RaspberryPi_Setup/ex01.php
@@ -1,1 +1,12 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>PHP Test</title>
+</head>
+<body>
+Today is <?php␣echo␣date('Y年m月d日'); ␣?>です。
+<h2>PHPのシステム情報表示</h2>
+<?php␣phpinfo();␣?>
+</body>
+</html>
 

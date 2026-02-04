7ec7b28d80ef5b5099d86ba0b9f7281125cd49cc
岩﨑 洋平 <35583461+youiwa@users.noreply.github.com>
2026-02-04 11:53:26 +0900

    Add web server setup script for Raspberry Pi
    Added commands to set up Apache web server and PHP on Raspberry Pi.

--- IEE1/RaspberryPi_Setup/web_setup.sh
+++ IEE1/RaspberryPi_Setup/web_setup.sh
@@ -1,1 +1,22 @@
+# Update & Upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
+# Webサーバ（Aparche2）のインストール
+sudo apt install apache2 -y
+sudo systemctl start apache2
+
+# ユーザディレクトリの有効化（Web）
+sudo a2enmod userdir
+sudo systemctl restart apache2
+
+# public_htmlの作成とパーミッション設定
+cd
+mkdir public_html
+chmod 755 public_html
+chmod 711 $HOME
+
+# PHPのインストール
+sudo apt install php -y
+sudo apt install php-fpm
+sudo systemctl restart apache2
